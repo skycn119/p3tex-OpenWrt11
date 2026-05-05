9e9bda467d0c7685303c19dfebea753604d23776
skycn119 <skycn0743@hotmail.com>
2026-05-05 10:01:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,18 +18,3 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# 确保当前目录是 openwrt 源码根目录（工作流已保证）
-echo "Current directory: $(pwd)"  # 调试用，可删除
-
-# 创建 files 目录层级
-mkdir -p files/usr/sbin
-mkdir -p files/lib/netifd/proto
-
-cp "$GITHUB_WORKSPACE/pppd" files/usr/sbin/pppd
-chmod +x files/usr/sbin/pppd
-cp "$GITHUB_WORKSPACE/pppd" files/lib/netifd/proto/ppp.sh
-chmod +x files/lib/netifd/proto/ppp.sh
-# 可选：检查文件是否复制成功
-ls -l files/usr/sbin/pppd
-
